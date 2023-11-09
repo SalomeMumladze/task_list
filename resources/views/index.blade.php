4055976f86331d05dcba10bd785f9c13cd24674b
salomeMumladze <salomemumladze775@gmail.com>
2023-11-09 14:41:55 +0400

    created fake data

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+
+<div>
+    @forelse ($tasks as $task)
+    <a href="{{route('task.show', ['id'=> $task->id])}}">
+        <p>{{$task->title}}</p>
+    </a>
+    @empty
+        <p>no tasks</p>
+    @endforelse
+</div>
+
+@endsection
