f10ba6e8d7d19414e260d2216efe7babcc7208f3
salomeMumladze <salomemumladze775@gmail.com>
2023-11-10 13:15:23 +0400

    save old value after error

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,6 +2,15 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+  <style>
+    .error-message {
+      color: red;
+      font-size: 0.8rem;
+    }
+  </style>
+@endsection
+
 @section('content')
     <form method='POST' action="{{route('tasks.store')}}">
         @csrf
@@ -9,17 +18,26 @@
             <label for="title">
             Title
             </label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title"  value="{{old('title')}}"/>
+            @error('title')
+            <p class="error-message">{{ $message }}</p>
+          @enderror
         </div>
 
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">{{old('description')}}</textarea>
+            @error('description')
+            <p class="error-message">{{ $message }}</p>
+          @enderror
         </div>
 
         <div>
             <label for="description2">Long Description</label>
-            <textarea name="description2" id="description2" rows="10"></textarea>
+            <textarea name="description2" id="description2" rows="10">{{old('description2')}}</textarea>
+            @error('description2')
+            <p class="error-message">{{ $message }}</p>
+          @enderror
         </div>
 
         <div>
